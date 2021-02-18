af29fea0606716a1eee9191fc79c88e7189ecd5f
YujianDiao <38806138+YujianDiao@users.noreply.github.com>
2021-02-18 12:54:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PIRACY import PIRACY
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("--step",  type=str, required=True)
     parser.add_argument("--datapath", type=pathlib.Path, required=True)
     parser.add_argument("--subject", type=str, required=True)
     parser.add_argument("--ses", type=int, required=True)
@@ -22,12 +23,22 @@ if __name__ == "__main__":
     myPiracy.anat_brain_masking()
     myPiracy.anat_reg_to_atlas()
     #fMRI
-    myPiracy.fmri_scale_pixdim_x10()    
-    myPiracy.fmri_loose_mask()
-    myPiracy.fmri_mpdenoising()
-    myPiracy.fmri_topup()
-    myPiracy.fmri_tight_mask()
-    myPiracy.fmri_reg_to_anat()
-    myPiracy.fmri_slicetiming_correction_spatial_smoothing()
-    myPiracy.fmri_ICA(components=40, ndelete=10)
-    myPiracy.create_connectome(gsr=True)
+    ##1. Preprocessing
+    if args.step == 'preprocessing':
+        myPiracy.fmri_scale_pixdim_x10()    
+        myPiracy.fmri_loose_mask()
+        myPiracy.fmri_mpdenoising()
+        myPiracy.fmri_topup()
+        myPiracy.fmri_tight_mask()
+        myPiracy.fmri_reg_to_anat()
+        myPiracy.fmri_slicetiming_correction_spatial_smoothing()
+        myPiracy.fmri_ICA(components=40, ndelete=10)
+        myPiracy.fix_classify(thresholds=[20, 70])
+    ##2. FIX cleaning. Manually create the noise file "fix_noise_file.txt" before proceeding.
+    elif args.step == 'cleaning':
+        myPiracy.fix_clean("fix_noise_file.txt")
+    ##3. FC generation
+    elif args.step == 'connectome':
+        myPiracy.create_connectome(gsr=True)
+    else:
+        print("Please choose 'step' from options: 'preprocessing', 'cleaning' and 'connectome'")
